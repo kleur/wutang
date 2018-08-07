1bb7076efc627b2ea30bedcd6df9389b2e9f7fc4
Koen van de Leur <koen.van.de.leur@osudio.com>
2018-08-07 14:05:45 +0200

    add test data

--- wu/core.py
+++ wu/core.py
@@ -3,8 +3,8 @@ from wu import helpers
 from wu import strategy
 
 def execute_me():
-    categories = helpers.init_categories('input/categories.yaml')
-    records = helpers.init_records('input/finance_records.csv')
+    categories = helpers.init_categories('test_input/categories.yaml')
+    records = helpers.init_records('test_input/finance_records.csv')
     records_categorized = apply_categories(categories, records)
     units = strategy.split_months(records_categorized)
     helpers.print_units(units)
